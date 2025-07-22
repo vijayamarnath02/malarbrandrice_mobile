1b7dc0ec035223396afbd29400a617a0fd2bae28
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-07-22 23:23:58 +0530

    Added new ui

--- src/app/tabs/page/dashboard/dashboard.page.ts
+++ src/app/tabs/page/dashboard/dashboard.page.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from "@ionic/angular";
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonContent, IonicModule]
 })
 export class DashboardPage implements OnInit {
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
